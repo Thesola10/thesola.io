e9ba6b7f2da620ed9673ad8d3725b7c0f5c69e9f
TheSola10 <me@thesola.io>
2024-03-22 11:59:09 +0100

    fixed wsgi entrypoint

--- backend/thesola_io_api/wsgi.py
+++ backend/thesola_io_api/wsgi.py
@@ -1,6 +1,9 @@
-from . import app
+from . import app as application
 from dotenv import load_dotenv
 
+import os
+
+load_dotenv(os.getenv("API_ENVFILE"))
+
 if __name__ == '__main__':
-    load_dotenv(os.getenv("API_ENVFILE"))
-    app.run()
+    application.run()
