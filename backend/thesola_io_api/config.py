c767e7ffefdb72b9d667ab296865105f1f06c7f9
TheSola10 <me@thesola.io>
2026-09-07 18:57:31 +0900

    Branching off now-playing to its own repo

--- backend/thesola_io_api/config.py
+++
@@ -1,2 +0,0 @@
-DEBUG = False
-MAIL_FROM_EMAIL = "admin@thesola.io"
--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 { description = "My personal website";
 
   inputs."nixpkgs".url = github:NixOS/nixpkgs;
+  inputs."nowplaying".url = github:thesola10/thesola.io-nowplaying;
   inputs."pelicanTheme".url = github:thesola10/pelican-thesola.io;
   inputs."pelicanTheme".flake = false;
 
@@ -9,11 +10,10 @@
     (system:
     let pkgs = import nixpkgs { inherit system; };
     in rec
-    { packages.backend = pkgs.callPackage ./backend {};
-      packages.default = pkgs.callPackage ./. { inherit pelicanTheme; };
+    { packages.default = pkgs.callPackage ./. { inherit pelicanTheme; };
       packages.publish = pkgs.callPackage ./. { inherit pelicanTheme; publish = true; };
       devShells.default = pkgs.mkShell {
-        inputsFrom = [ packages.backend packages.default ];
+        inputsFrom = [ packages.default ];
         packages = with pkgs; [ darkhttpd ];
         buildInputs = with pkgs.python3Packages; [
           venvShellHook
@@ -21,6 +21,5 @@
 
         venvDir = ".venv";
       };
-    })
-  // { nixosModules.default = import ./backend/nixos; };
+    });
 }
